4a01db954d57b0bfaa9702aca9270f056eea42c3
André Pedersen <andrped94@gmail.com>
2024-10-13 16:23:46 +0200

    Implemented singleton to allow shared memory between sessions

---
+++ postly/clients/singleton_client.py
@@ -0,0 +1,10 @@
+from .postly_client import PostlyClient
+
+class SingletonPostlyClient:
+    _instance = None
+
+    @staticmethod
+    def get_instance():
+        if SingletonPostlyClient._instance is None:
+            SingletonPostlyClient._instance = PostlyClient()
+        return SingletonPostlyClient._instance
