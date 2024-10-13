293aa344b5511f185ff8ce7abd2778f8cb3c75ee
André Pedersen <andrped94@gmail.com>
2024-10-13 14:00:03 +0200

    include app.py in linting

--- shell/format.sh
+++ shell/format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-isort --sl postly/
-black --line-length 120 .
-flake8 --max-line-length 120 .
+isort --sl postly/ app.py
+black --line-length 120 postly/ app.py
+flake8 --max-line-length 120 postly/ app.py
--- shell/lint.sh
+++ shell/lint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-isort --check --sl -c .
+isort --check --sl -c postly/ app.py
 if ! [ $? -eq 0 ]
 then
   echo "Please run \"sh shell/format.sh\" to format the code."
   exit 1
 fi
 echo "no issues with isort"
-flake8 --max-line-length 120 .
+flake8 --max-line-length 120 postly/ app.py
 if ! [ $? -eq 0 ]
 then
   echo "Please fix the code style issue."
   exit 1
 fi
 echo "no issues with flake8"
-black --check --line-length 120 .
+black --check --line-length 120 postly/ app.py
 if ! [ $? -eq 0 ]
 then
   echo "Please run \"sh shell/format.sh\" to format the code."
