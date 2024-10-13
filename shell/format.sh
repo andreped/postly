6bd9ae0490f51fceb9f7ff6ed5a4cbda27155452
André Pedersen <andrped94@gmail.com>
2024-10-13 14:01:01 +0200

    Include tests/ in lint

--- shell/format.sh
+++ shell/format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-isort --sl postly/ app.py
-black --line-length 120 postly/ app.py
-flake8 --max-line-length 120 postly/ app.py
+isort --sl postly/ tests/ app.py
+black --line-length 120 postly/ tests/ app.py
+flake8 --max-line-length 120 postly/ tests/ app.py
