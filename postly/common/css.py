f7239ac20a4a7972398f39ca88ce1fcfb9711f52
André Pedersen <andrped94@gmail.com>
2024-10-14 19:48:33 +0200

    Added CSS styling for UI

---
+++ postly/common/css.py
@@ -0,0 +1,36 @@
+def get_theme():
+    return """
+    <style>
+    body {
+        background-color: #ffffff;
+        color: #1DA1F2;
+    }
+    .stButton>button {
+        background-color: #1DA1F2;
+        color: white;
+    }
+    .stTextInput>div>div>input {
+        border: 1px solid #1DA1F2;
+    }
+    .stTextArea>div>div>textarea {
+        border: 1px solid #1DA1F2;
+    }
+    .stSelectbox>div>div>div>div {
+        border: 1px solid #1DA1F2;
+    }
+    .post-container {
+        border: 1px solid #1DA1F2;
+        border-radius: 10px;
+        padding: 10px;
+        margin-bottom: 10px;
+        background-color: #f5f8fa;
+    }
+    .post-header {
+        font-weight: bold;
+        color: #1DA1F2;
+    }
+    .post-content {
+        color: #14171A;
+    }
+    </style>
+    """
